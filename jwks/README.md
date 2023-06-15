8120265a062c46cd6e8718e0837f6616023828d1
Eric Debeij <hdebeij@akamai.com>
2023-06-15 21:41:56 +0200

    JWKS-first version

---
+++ jwks/README.md
@@ -0,0 +1,9 @@
+# JWKS at the Akamai Edge
+
+**DISCLAIMER: This code is designed to be used as an example only. No guarantees are made that it's fit for purpose. It's not production ready and should not be used to protect critical resources**
+
+This example can be used to verifiy a JSON WebToken. To verify the token the public certificate will be retrieved from an external location.
+
+The documentation of this example is not ready yet.
+
+The code is based on the JWT authentication example in the [Akamai edgeworker-examples repository](https://github.com/akamai/edgeworkers-examples/tree/master/edgecompute/examples/authentication/jwt)
---
+++ jwks/edgeworker/main.js
@@ -0,0 +1,54 @@
+import { logger } from 'log';
+import { httpRequest } from 'http-request';
+import { JWTValidator } from './jwt.js';
+import { crypto} from 'crypto';
+import { Cookies } from 'cookies';
+
+//advanced options for jwt validator
+const jwtOption = {
+  //check token expiry
+  ignoreExpiration: false,
+  //check token nbf
+  ignoreNotBefore: true
+};
+
+const jwtValidator = new JWTValidator(jwtOption);
+
+export async function onClientRequest(request) {
+  try {
+    var certResponse = await httpRequest(`${request.scheme}://${request.host}/oidc/certs`);
+    var certs = await certResponse.json(); 
+    
+    var cookies = new Cookies(request.getHeader('Cookie'));
+    let jwtToken = request.getHeader('jwt');
+    if (!jwtToken) jwtToken = cookies.get("__jwt__");
+    if (!jwtToken) throw new Error('token required');
+    
+    let keys = [];
+    for (let i = 0; i < certs.keys.length; ++i) {
+      const c = certs.keys[i];
+
+      const iKey = await crypto.subtle.importKey(
+        'jwk',
+        c,
+        {
+          name: 'RSASSA-PKCS1-v1_5',
+          hash: 'SHA-256'
+        },
+        false,
+        ['verify']
+      );
+      keys.push(iKey)
+    }
+    const jwtJSON = await jwtValidator.validate(jwtToken, keys);
+    const result = {
+      jwt: jwtJSON,
+      verified: true
+    };
+    request.respondWith(200, {"Content-Type": ["application/json"]}, JSON.stringify(result));
+
+  } catch (error) {
+    logger.log('Error: %s', error.message);
+    request.respondWith(400, {}, error.message);
+  }
+}
